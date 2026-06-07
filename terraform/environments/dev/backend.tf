8d167e988007f3d6f4fae1dee193c0449d34929e
FrankAsanteVanLaarhoven <frankleroyvan@gmail.com>
2026-06-07 02:58:33 +0100

    chore: patch backend bucket with real account ID, add tfvars example
    Account: 158709926611. S3 bucket: petclinic-terraform-state-158709926611.

--- terraform/environments/dev/backend.tf
+++ terraform/environments/dev/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "petclinic-terraform-state-ACCOUNT_ID"
+    bucket         = "petclinic-terraform-state-158709926611"
     key            = "petclinic/dev/terraform.tfstate"
     region         = "eu-central-1"
     encrypt        = true
